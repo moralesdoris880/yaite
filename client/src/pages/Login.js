1b0b6f4116cf0ab722cf5221845dd9968a09c4a0
Doris Morales <moralesdoris880@gmail.com>
2022-05-31 10:59:28 -0400

    login form elements added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,15 @@
 function Login(){
     return(
         <div>
-
+            <form>
+                <input type="text" placeholder="Username" ></input>
+                <input type="text" placeholder="Password" ></input>
+                <input type="submit" value="Login"></input>
+            </form>
         </div>
     );
 }
 
 export default Login;
+
+// onChange={(e) => setPassword(e.target.value)}
