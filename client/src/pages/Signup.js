b00e0164ef59dcafbad48fbc4e23d6576d174669
Doris Morales <moralesdoris880@gmail.com>
2022-05-31 11:00:24 -0400

    signup form elements added

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,12 @@
 function Signup(){
     return(
         <div>
-
+            <form>
+                <input type="text" placeholder="Username" ></input>
+                <input type="text" placeholder="Password" ></input>
+                <input type="text" placeholder="Re-Enter Password" ></input>
+                <input type="submit" value="Sign Up"></input>
+            </form>
         </div>
     );
 }
