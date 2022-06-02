a010d3dff1499fbb2c7afb47c31caa275d435595
Doris Morales <moralesdoris880@gmail.com>
2022-06-02 13:30:06 -0400

    Signup css done

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,11 +5,11 @@ function Login(){
         <div id="Login">
             <div className="Header"><h1>Yaite</h1></div>
             <form id="loginform">
-                <h1>Login</h1>
-                <p>Don't have an account? <a href='/signup'>Sign up</a> for free.</p>
-                <input type="text" placeholder="Username" ></input>
-                <input type="text" placeholder="Password" ></input>
-                <input type="submit" value="Login"></input>
+                <h1 className='heading'>Log In</h1>
+                <p className='text' style={{textAlign:'center'}}>Don't have an account? <a href='/signup'>Sign up</a> for free.</p>
+                <input type="text" placeholder="Username" className='inputs' ></input>
+                <input type="text" placeholder="Password" className='inputs'></input>
+                <input type="submit" value="Log In" className='submit'></input>
             </form>
         </div>
     );
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,12 +1,16 @@
+import '../css/Signup.css'
+
 function Signup(){
     return(
         <div id="Signup">
             <div className="Header"><h1>Yaite</h1></div>
-            <form>
-                <input type="text" placeholder="Username" ></input>
-                <input type="text" placeholder="Password" ></input>
-                <input type="text" placeholder="Re-Enter Password" ></input>
-                <input type="submit" value="Sign Up"></input>
+            <form id="signupform">
+                <h1 className='heading'>Sign Up</h1>
+                <p className='text' style={{textAlign:'center'}}>Already have an account? <a href='/login'>Login</a></p>
+                <input type="text" placeholder="Username" className='inputs' ></input>
+                <input type="text" placeholder="Password" className='inputs' ></input>
+                <input type="text" placeholder="Re-Enter Password" className='inputs' ></input>
+                <input type="submit" value="Sign Up" className='submit'></input>
             </form>
         </div>
     );
