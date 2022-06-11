423134d89641b66d6163f805068cf9b39aaf289b
Doris Morales <moralesdoris880@gmail.com>
2022-06-11 16:18:37 -0400

    Show password checkbox added

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -6,6 +6,7 @@ function Signup(){
     const[password,setPassword]=useState("");
     const[passwordConfirmation,setPasswordConfirmation]=useState("");
     const[username,setUsername]=useState("");
+    const[displayPassword,setDisplayPassword]=useState(true);
 
     function handleSignup(e){ 
         e.preventDefault();
@@ -28,15 +29,21 @@ function Signup(){
           });
     }
 
+    function handleDisplayPassword(e){
+      e.preventDefault();
+      setDisplayPassword(!displayPassword)
+    }
+
     return(
         <div id="Signup">
             <div className="Header"><h1>Yaite</h1></div>
             <form id="signupform" onSubmit={handleSignup}>
                 <h1 className='heading'>Sign Up</h1>
                 <p className='text' style={{textAlign:'center'}}>Already have an account? <a href='/login'>Login</a></p>
-                <input type="text" placeholder="Username" className='inputs' onChange={(e) => setUsername(e.target.value)}></input>
-                <input type="text" placeholder="Password" className='inputs' onChange={(e) => setPassword(e.target.value)}></input>
-                <input type="text" placeholder="Re-Enter Password" className='inputs' onChange={(e) => setPasswordConfirmation(e.target.value)}></input>
+                <input type="text" placeholder="Username" className='inputs' onChange={(e) => setUsername(e.target.value)} required></input>
+                <input type={displayPassword? "text":"password"} placeholder="Password" className='inputs' onChange={(e) => setPassword(e.target.value)} required></input>
+                <input type={displayPassword? "text":"password"} placeholder="Re-Enter Password" className='inputs' onChange={(e) => setPasswordConfirmation(e.target.value)} required></input>
+                <input type="checkbox" onClick={(e) => handleDisplayPassword(e)}/>Show Password
                 <input type="submit" value="Sign Up" className='submit'></input>
             </form>
         </div>
