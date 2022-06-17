cc7ac7e8ad1eb3d142999698c0d94b3d9c02c925
Doris Morales <moralesdoris880@gmail.com>
2022-06-17 16:37:58 -0400

    Routes added for messages and chatrooms controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :messages
+  resources :chatrooms
+
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/me', to: 'users#show'
+  get '/chatroom_messages', to: 'messages#chatroom_messages'
 
   get '*path',
   to: 'fallback#index',
