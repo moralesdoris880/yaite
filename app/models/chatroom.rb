71959de28be08730fedef05f330fdfbf15b151e9
Doris Morales <moralesdoris880@gmail.com>
2022-06-21 13:22:22 -0400

    chatroom model validation added

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,4 +3,5 @@ class Chatroom < ApplicationRecord
     has_many :users, through: :messages
     #Inverse allows the chatroom to stay memorized when sending messages to it. So it doesnt send msgs to another chatroom.
     #Dependent destroy - deletes current record and its associations aka messages
+    validates :name, presence: true, uniqueness: true
 end
