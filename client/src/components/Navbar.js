c29253da974100604bb10eb1973fe6da1328931b
Doris Morales <moralesdoris880@gmail.com>
2022-06-21 16:30:27 -0400

    logout fetch added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import '../css/Navbar.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../features/reducers/authSlice';
 
 function Navbar(){
     const[burgerNav,setBurgerNav]= useState(true)
     const navigate = useNavigate();
+    const auth = useSelector((state) => state.auth.value)
+    const dispatch = useDispatch();
 
-    // function handleLogout(){
-    //     fetch("/logout", { method: "DELETE" }).then((r) => { 
-    //         if (r.ok) {
-    //           setUser(null); PASS THRU REDUX
-    //         }
-    //       });
-    // }
+
+    function handleLogout(e){
+        e.preventDefault();
+        fetch("/logout", { method: "DELETE" }).then((r) => { 
+            if (r.ok) {
+             dispatch(logout());
+            }
+        });
+    }
 
     return(
         <div id="Navbar">
@@ -22,8 +28,8 @@ function Navbar(){
                 <button onClick={()=>navigate("/")} className="navtab"><span className="material-symbols-outlined" style={{ fontSize:'24px'}}>home</span></button>
                 <button onClick={()=>navigate("/about")} className="navtab"><span className="material-symbols-outlined" style={{ fontSize:'24px'}}>info</span></button>
                 <button onClick={()=>navigate("/settings")} className="navtab"><span className="material-symbols-outlined" style={{ fontSize:'24px'}}>settings</span></button>
-                <span className="material-symbols-outlined" style={{ fontSize:'24px'}}>exit_to_app</span>
             </ul>}
+            {auth?<button onClick={(e)=>handleLogout(e)} className="navtab"><span className="material-symbols-outlined" style={{ fontSize:'24px'}}>exit_to_app</span></button>:console.log('not logged in')}
         </div>
     );
 }
