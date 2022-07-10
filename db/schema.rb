912cbb160f4eccebd6e6c2d83bb7b2f13d1c6ca0
Doris Morales <moralesdoris880@gmail.com>
2022-07-10 19:19:25 -0400

    everything back to normal :)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_19_151531) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_10_231058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.string "alt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
